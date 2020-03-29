54ddc32c5a7c124c3a6504acdb4b3dd44fb59e30
Trần Hữu Lộc <loctran0169@gmail.com>
2020-03-29 16:16:37 +0700

    update icon notify

--- app/src/main/java/vn/edu/uit/managementforstudents/data/adapters/AdapterNotifyPerson.kt
+++ app/src/main/java/vn/edu/uit/managementforstudents/data/adapters/AdapterNotifyPerson.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import vn.edu.uit.managementforstudents.R
 import vn.edu.uit.managementforstudents.data.models.NotifyPerson
 
 class AdapterNotifyPerson(val context: Context, var list: List<NotifyPerson>) : RecyclerView.Adapter<AdapterNotifyPerson.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterNotifyPerson.ViewHolder {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.item_notify_person,parent,false)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.item_notify_person, parent, false)
         return ViewHolder(view)
     }
 
@@ -19,10 +20,11 @@ class AdapterNotifyPerson(val context: Context, var list: List<NotifyPerson>) : 
     }
 
     override fun onBindViewHolder(holder: AdapterNotifyPerson.ViewHolder, position: Int) {
-
+        if (position % 2 != 0)
+            holder.image.isSelected = true
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val image = view.findViewById<ImageButton>(R.id.img_isRead)
     }
 }
