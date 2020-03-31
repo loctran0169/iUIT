fd248a18c301e8f95b7f0f722a0f64db6f2c3f0e
Trần Hữu Lộc <loctran0169@gmail.com>
2020-04-01 00:01:38 +0700

    demo bottom sheet history

--- app/src/main/java/vn/edu/uit/managementforstudents/data/adapters/AdapterHistory.kt
+++ app/src/main/java/vn/edu/uit/managementforstudents/data/adapters/AdapterHistory.kt
@@ -1,16 +1,20 @@
 package vn.edu.uit.managementforstudents.data.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import vn.edu.uit.managementforstudents.R
 
 class AdapterHistory : RecyclerView.Adapter<AdapterHistory.ViewHolder>() {
-
+    lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterHistory.ViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_history, parent, false)
+        context = parent.context
         return ViewHolder(view)
     }
 
@@ -24,10 +28,16 @@ class AdapterHistory : RecyclerView.Adapter<AdapterHistory.ViewHolder>() {
             holder.tvLeave.visibility = View.GONE
         } else {
             holder.tvEnjoy.visibility = View.GONE
-            holder.tvLeave.visibility=View.VISIBLE
+            holder.tvLeave.visibility = View.VISIBLE
         }
         if (position % 3 == 0) {
-            holder.tvProto.text="Class"
+            holder.tvProto.text = "Class"
+        }
+        holder.layout.setOnClickListener {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_history, null)
+            val dialog = BottomSheetDialog(context)
+            dialog.setContentView(view)
+            dialog.show()
         }
     }
 
@@ -35,5 +45,6 @@ class AdapterHistory : RecyclerView.Adapter<AdapterHistory.ViewHolder>() {
         val tvEnjoy = view.findViewById<TextView>(R.id.tvEnjoyClass)
         val tvLeave = view.findViewById<TextView>(R.id.tvLeaveClass)
         val tvProto = view.findViewById<TextView>(R.id.lbProto)
+        val layout = view.findViewById<ConstraintLayout>(R.id.layout_history)
     }
 }
